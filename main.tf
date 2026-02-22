d7a69678b26cd76561aa0eeec73b33ced061f70c
emmanuelfontem <emmanuelfontem81@gmail.com>
2026-02-21 22:40:17 -0600

    modified main.tf file

--- main.tf
+++ main.tf
@@ -3,10 +3,40 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-09256c524fab91d36"
-  instance_type = "t2.micro"
+  ami                    = "ami-09256c524fab91d36"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<EOF
+#!/bin/bash
+yum update -y
+yum install -y httpd
+systemctl enable httpd
+systemctl start httpd
+echo "Hello, world" > /var/www/html/index.html
+EOF
+
+  user_data_replace_on_change = true
 
   tags = {
     Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
